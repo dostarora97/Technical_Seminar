9d1333f5b669564b66bc6a0623d71bae62bcde5b
Dost Arora <31973060+dostarora97@users.noreply.github.com>
2020-05-18 15:09:00 +0530

    Add files via upload

---
+++ Script_Technical_Seminar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/dostarora97/Technical_Seminar.git  
+cd Technical_Seminar  
+python3 -m venv Env_TechSem  
+source Env_TechSem/bin/activate  
+pip freeze > tmp.txt
+pip uninstall -r tmp.txt -y
+python -m pip install --upgrade pip 
+pip install -U wheel
+pip install -U setuptools
+pip install -r requirements.ubuntu.txt  
+jupyter notebook  
