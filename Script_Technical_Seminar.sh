4d62ed2113bc40b77b1c5dedb6053e2b542c3303
dostarora97 <dost.arora97@gmail.com>
2020-05-20 17:49:15 +0530

    Install Python3.8 and complete setup

--- Script_Technical_Seminar.sh
+++ Script_Technical_Seminar.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
-
+sudo apt-get install software-properties-common
+echo "##### STEP - 1/16 #####"
+sudo add-get repository ppa:deadsnakes/ppa
+echo "##### STEP - 2/16 #####"
+sudo apt-get update
+echo "##### STEP - 3/16 #####"
+sudo apt-get install python3.8 -y
+echo "##### STEP - 4/16 #####"
+sudo apt install python3-pip -y
+echo "##### STEP - 5/16 #####"
+sudo apt install python3.8-venv -y
+echo "##### STEP - 6/16 #####"
+sudo apt-get autoremove - y
+echo "##### STEP - 7/17 #####"
 git clone https://github.com/dostarora97/Technical_Seminar.git  
+echo "##### STEP - 8/17 #####"
 cd Technical_Seminar  
-python3 -m venv Env_TechSem  
+echo "##### STEP - 9/17 #####"
+python3.8 -m venv Env_TechSem  
+echo "##### STEP - 10/17 #####"
 source Env_TechSem/bin/activate  
-pip freeze > tmp.txt
-pip uninstall -r tmp.txt -y
-python -m pip install --upgrade pip 
-pip install -U wheel
-pip install -U setuptools
-pip install -r requirements.ubuntu.txt  
-jupyter notebook  
+echo "##### STEP - 11/17 #####"
+python3.8 -m pip freeze > tmp.txt
+echo "##### STEP - 12/17 #####"
+python3.8 -m pip uninstall -r tmp.txt -y
+echo "##### STEP - 13/17 #####"
+python3.8 -m pip install --upgrade pip 
+echo "##### STEP - 14/17 #####"
+python3.8 -m pip install -U wheel
+echo "##### STEP - 15/17 #####"
+python3.8 -m pip install -U setuptools
+echo "##### STEP - 16/17 #####"
+python3.8 -m pip install -r requirements.ubuntu.txt  
+echo "##### STEP - 17/17 #####"
+python3.8 -m jupyter notebook  
